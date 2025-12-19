610b4caaa5b9a7f219175ed64d2d75f279edd8d4
Suryakant Singh <surykntsingh@gmail.com>
2025-12-19 15:10:04 +0530

    Updated dataset

--- main.py
+++ main.py
@@ -37,9 +37,9 @@ def parse_agrs():
 
 
     # Model settings (for Transformer)
-    parser.add_argument('--d_model', type=int, default=512, help='the dimension of Transformer.')
-    parser.add_argument('--d_ff', type=int, default=512, help='the dimension of FFN.')
-    parser.add_argument('--d_vf', type=int, default=1024, help='the dimension of the patch features.')
+    parser.add_argument('--d_model', type=int, default=768, help='the dimension of Transformer.')
+    parser.add_argument('--d_ff', type=int, default=768, help='the dimension of FFN.')
+    parser.add_argument('--d_vf', type=int, default=768, help='the dimension of the patch features.')
     parser.add_argument('--num_heads', type=int, default=4, help='the number of heads in Transformer.')
     parser.add_argument('--num_layers', type=int, default=3, help='the number of layers of Transformer.')
     parser.add_argument('--dropout', type=float, default=0.1, help='the dropout rate of Transformer.')
